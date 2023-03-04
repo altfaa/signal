d5788cde8dfce8a56dd96bd88e49342127b6696d
a <a@>
2023-03-04 15:02:57 +0300

    1

--- kit/telegram_api/send.py
+++ kit/telegram_api/send.py
@@ -5,6 +5,7 @@ from telebot import TeleBot
 
 def send_photo_to_my_channel(photo_fp, text_message):
     try:
+        print(101)
         bot = TeleBot(telegram_token_A)
         bot.send_photo(CHANNEL_NAME, photo_fp, text_message, parse_mode='Markdown')
         bot.close()
