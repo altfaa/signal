49b9b0acd8c1369a3c994ac2ca92a728781386df
a <a@>
2023-03-04 15:16:04 +0300

    1

--- regular/checker_5m.py
+++ regular/checker_5m.py
@@ -24,7 +24,10 @@ for ticker in ticker_to_figi:
     r, c = df.shape
     last_date = df['Date'].iloc[-1]
     last_date_datetype = datetime.strptime(last_date, '%Y-%m-%d %H:%M')
+    now_date = datetime.now()
 
+    time_delta = now_date - last_date
+    print("DELTA ", time_delta)
     now_hour = datetime.now().hour
 
     print("этот час", now_hour, "час из df", last_date_datetype.hour)
