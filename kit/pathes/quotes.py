776ae7df9408aa303140ff27656724c604581b23
a <a@>
2023-03-04 12:23:24 +0300

    1

--- kit/pathes/quotes.py
+++ kit/pathes/quotes.py
@@ -2,6 +2,6 @@ from pathlib import Path
 
 dirname = Path(__file__).parents[1]
 
-DB_NAME_QUOTES_1H = Path.join(dirname, 'db', 'quotes.db')
-DB_NAME_QUOTES_15M = Path.join(dirname, 'db', 'quotes_15.db')
-DB_NAME_QUOTES_5M = Path.join(dirname, 'db', 'quotes_5.db')
+DB_NAME_QUOTES_1H = Path(dirname, 'db', 'quotes.db')
+DB_NAME_QUOTES_15M = Path(dirname, 'db', 'quotes_15.db')
+DB_NAME_QUOTES_5M = Path(dirname, 'db', 'quotes_5.db')
