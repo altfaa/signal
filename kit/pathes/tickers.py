080195f49946912cf41ba5061bb170ef574597ef
a <a@>
2023-03-04 12:22:45 +0300

    1

--- kit/pathes/tickers.py
+++ kit/pathes/tickers.py
@@ -1,7 +1,9 @@
 from pathlib import Path
 
 
-dirname = Path(__file__).parents[0]
+dirname = Path(__file__).parents[1]
 
 path_full_moex = Path(dirname, 'catalog', 'full_moex')
 path_short_moex = Path(dirname, 'catalog', 'short_moex')
+print(dirname)
+print(path_full_moex)
