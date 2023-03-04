dac3fb042ab62a379fd7d1cfbd047648e360a85a
a <a@>
2023-03-05 00:36:10 +0300

    1

--- regular/volumer_plot.py
+++ regular/volumer_plot.py
@@ -3,8 +3,8 @@ from kit.plot.volume_day import good_make_table_volumes
 from kit.telegram_api.send import send_photo_to_my_channel
 from datetime import datetime, timedelta
 
-date_str_param = str(datetime.now() - timedelta(days=1))[:10]
-date_string_title = (datetime.now() - timedelta(days=1)).strftime("%d %b %Y")
+date_str_param = str(datetime.now() - timedelta(days=2))[:10]
+date_string_title = (datetime.now() - timedelta(days=2)).strftime("%d %b %Y")
 
 df, info_list = get_df_all_volumes(date_str_param)
 
