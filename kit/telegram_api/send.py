61f1012be3a08f7907a3807f10cc1e7bb9d85c11
a <a@>
2023-03-04 15:07:09 +0300

    1

--- kit/telegram_api/send.py
+++ kit/telegram_api/send.py
@@ -8,7 +8,7 @@ def send_photo_to_my_channel(photo_fp, text_message):
     try:
         print(101)
         bot = TeleBot(telegram_token_A)
-        bot.send_photo(CHANNEL_NAME, photo_fp, text_message, parse_mode='Markdown')
+        bot.send_photo(chat_id=CHANNEL_NAME, photo=photo_fp, caption=text_message, parse_mode='Markdown')
         bot.close()
     except Exception as e:
         print(e)
