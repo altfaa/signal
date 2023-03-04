4abc14b5234ef76f14a0d24251fd0cb737797232
a <a@>
2023-03-04 21:29:04 +0300

    1

--- regular/volumer_plot.py
+++ regular/volumer_plot.py
@@ -1,12 +1,15 @@
 from kit.sql_api.get_df.volumer import get_df_all_volumes
 from kit.plot.volume_day import good_make_table_volumes
 from kit.telegram_api.send import send_photo_to_my_channel
+from datetime import datetime, timedelta
 
 
-date = '2023-02-27'
-df = get_df_all_volumes(date)
+date_str_param = str(datetime.now() - timedelta(days=1))[:10]
+date_string_title = (datetime.now() - timedelta(days=1)).strftime("%d %b %Y %H:%M")
+
+df = get_df_all_volumes(date_str_param)
 out_filename = "123.png"
-good_make_table_volumes(df=df, out_filename=out_filename, tittle=date)
+good_make_table_volumes(df=df, out_filename=out_filename, tittle=date_string_title)
 photo_fp = open(out_filename, 'rb')
 send_photo_to_my_channel(photo_fp=photo_fp, text_message="270223")
 photo_fp.close()
