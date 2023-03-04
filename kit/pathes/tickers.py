b26b3c183024daadb80e054ee8e6c36ae919f3e0
a <a@>
2023-03-04 12:02:53 +0300

    1

--- kit/pathes/tickers.py
+++ kit/pathes/tickers.py
@@ -2,5 +2,5 @@ import os
 pwd = os.getcwd()
 dirname = os.path.dirname(__file__)
 
-path_full_moex = os.path.join(dirname, 'catalog', 'full_moex')
-path_short_moex = os.path.join(dirname, 'catalog', 'short_moex')
+path_full_moex = os.path.join(dirname, '../catalog', 'full_moex')
+path_short_moex = os.path.join(dirname, '../catalog', 'short_moex')
