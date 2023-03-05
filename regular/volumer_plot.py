8ace2c95f13ff2e82bf0817745f0140cd78ca923
altfaa <altfaa@ya.ru>
2023-03-05 23:33:43 +0300

    1

--- regular/volumer_plot.py
+++ regular/volumer_plot.py
@@ -4,8 +4,8 @@ from kit.telegram_api.send import send_photo_to_my_channel
 from datetime import datetime, timedelta
 from kit.names.channel import CHANNEL_NAME
 
-date_str_param = str(datetime.now() - timedelta(days=2))[:10]
-date_string_title = (datetime.now() - timedelta(days=2)).strftime("%d %b %Y")
+date_str_param = str(datetime.now() - timedelta(days=1))[:10]
+date_string_title = (datetime.now() - timedelta(days=1)).strftime("%d %b %Y")
 
 df, info_list = get_df_all_volumes(date_str_param)
 
