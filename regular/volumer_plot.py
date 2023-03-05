8b273251da80ea176dd171a6f418eb22fc1048c6
altfaa <altfaa@ya.ru>
2023-03-05 23:13:49 +0300

    1

--- regular/volumer_plot.py
+++ regular/volumer_plot.py
@@ -14,14 +14,14 @@ vol = info_list[1]
 cost = info_list[2]
 total = info_list[3]
 
-cost_string = f"{cost}".replace(',', ' ')
+cost_string = f"{cost:,}".replace(',', ' ')
 total_string = f"{total / 1000000000:.1f}"
 
 text = f"Дневной оборот\n\n🗓 {date_string_title}\n\n __Σ__  {total_string} млрд.₽\n\n" + f"{CHANNEL_NAME}".replace("_",
-                                                                                                                   "\\_")
+                                                                                                                  "\\_")
 
 out_filename = "volume_hbar.png"
-good_make_table_volumes(df=df, out_filename=out_filename, tittle=date_string_title, info=[name, vol, cost])
+good_make_table_volumes(df=df, out_filename=out_filename, tittle=date_string_title, info=[name,vol,cost])
 photo_fp = open(out_filename, 'rb')
 send_photo_to_my_channel(photo_fp=photo_fp, text_message=text)
 photo_fp.close()
