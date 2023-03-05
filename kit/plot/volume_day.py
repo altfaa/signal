4cc53add8f76bfcb59a965f75f6afc3685c4d93f
a <a@>
2023-03-05 15:37:02 +0300

    1

--- kit/plot/volume_day.py
+++ kit/plot/volume_day.py
@@ -22,6 +22,14 @@ def good_make_table_volumes(df, out_filename, tittle):
     ax.set_title(f'Объемы торговли акциями MOEX {tittle}, %')
     ax.bar_label(ax.containers[0])
     ax.axes.get_xaxis().set_visible(False)
+
+    ax.text(0.01, 0.1, "SBER", horizontalalignment='left', verticalalignment='center', transform=ax.transAxes,
+             fontsize=16)
+    ax.text(0.01, 0.2, f"Volume: {454545454}", horizontalalignment='left', verticalalignment='center',
+             transform=ax.transAxes)
+    ax.text(0.83, 0.1, "@moex signal", horizontalalignment='left', verticalalignment='center', transform=ax.transAxes)
+
+
     plt.show()
     plt.savefig(out_filename, dpi=200, bbox_inches='tight')
     plt.close(fig)
